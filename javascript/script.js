ac6ddb6faac0adba21bf157d744d537c0fd5e288
Amanda <amandamariciaraphael@gmail.com>
2021-12-30 23:43:34 +0100

    updated sqrt

--- javascript/script.js
+++ javascript/script.js
@@ -8,17 +8,9 @@ const display = document.querySelector(".display");
 
 const ac = document.querySelector(".ac");
 
-const pm = document.querySelector(".pm");
+const squareRoot = document.querySelector(".squareRoot");
 
 const percent = document.querySelector(".percent");
-
-const addition = document.querySelector(".addition");
-
-const subtraction = document.querySelector(".subtraction");
-
-const multiplication = document.querySelector(".multiplication");
-
-const division = document.querySelector(".division");
 
 const equal = document.querySelector(".equal");
 
@@ -26,44 +18,64 @@ const decimal = document.querySelector(".decimal");
 const number = document.querySelectorAll(".number");
 const operator = document.querySelectorAll(".operator");
 let clicked = ``;
+let result = 0;
+const operandSymbols = `+*/-`;
+function displayClicked(e) {
+  clicked += e.target.textContent;
+  display.textContent = clicked;
+}
 number.forEach((num) => {
   num.addEventListener("click", function (e) {
-    clicked += e.target.textContent;
-
-    display.textContent = clicked;
+    if (result !== 0) {
+      result = 0;
+      clicked = ``;
+    }
+    displayClicked(e);
   });
 });
 
 operator.forEach((operatorElement) => {
   operatorElement.addEventListener("click", function (e) {
-    clicked += e.target.textContent;
-    document.querySelector(".display").textContent = clicked;
+    result = 0;
+    displayClicked(e);
   });
 });
 decimal.addEventListener("click", function () {
-  let operandSymbols = `+*/-`;
   if (clicked[0] == 0) {
     clicked = 0 + `.`;
-
-    document.querySelector(".display").textContent = clicked;
+    display.textContent = clicked;
   } else if (clicked[0] !== 0 || operandSymbols.includes(clicked)) {
     clicked += `.`;
-
-    document.querySelector(".display").textContent = clicked;
+    display.textContent = clicked;
   }
 });
 equal.addEventListener("click", function () {
-  let value = eval(clicked);
-  clicked = value;
-  document.querySelector(".display").textContent = clicked;
+  result = eval(clicked);
+  clicked = result;
+  display.textContent = clicked;
 });
 
 ac.addEventListener("click", function () {
   clicked = ``;
-  document.querySelector(".display").textContent = 0;
+  display.textContent = 0;
 });
 percent.addEventListener("click", function () {
-  let value = eval(clicked) / 100;
-    clicked = value;
-  document.querySelector(".display").textContent = value;
+  value = eval(clicked) / 100;
+  clicked = value.toFixed(4);
+  display.textContent = clicked;
+});
+squareRoot.addEventListener("click", function () {
+  let squareRootValue = 0;
+  if (squareRootValue === 0) {
+    squareRootValue = Math.sqrt(eval(clicked)).toFixed(2);
+    value = squareRootValue;
+    clicked = ``;
+  }
+  if (squareRootValue !== 0 && !operandSymbols.includes(clicked)) {
+    value = Math.sqrt(eval(squareRootValue)).toFixed(2);
+  } else {
+    squareRootValue = Math.sqrt(eval(clicked)).toFixed(2);
+  }
+  clicked = value;
+  display.textContent = clicked;
 });
